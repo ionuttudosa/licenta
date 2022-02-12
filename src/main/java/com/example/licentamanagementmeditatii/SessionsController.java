193e0b743ff901cd8fbc862187da59b0f940eeae
Ionut <ionuttudosa89@gmail.com>
2022-02-12 12:44:56 +0200

    Facut sa arate studenti in functie de idMeditator v2

--- src/main/java/com/example/licentamanagementmeditatii/SessionsController.java
+++ src/main/java/com/example/licentamanagementmeditatii/SessionsController.java
@@ -1,4 +1,5 @@
 package com.example.licentamanagementmeditatii;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -12,12 +13,51 @@ import java.util.ResourceBundle;
 public class SessionsController implements Initializable {
     @FXML
     private ComboBox<Integer> combobox_durata;
-
+    @FXML
+    private ComboBox<String> combobox_student;
+    @FXML
+    private Button button_logout;
+    @FXML
+    private Button button_back;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         combobox_durata.getItems().add(60);
         combobox_durata.getItems().add(90);
         combobox_durata.getItems().add(120);
+
+        ObservableList<UseriCati> students = Utils.getDatausers();
+        for (UseriCati student:students) {
+            combobox_student.getItems().add(student.getId()+" "+student.getNume()+" "+student.getPrenume());
+        }
+
+
+
+
+        button_logout.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+//                search_user();
+//                UpdateTable();
+                Utils.changeScene(event, "Landing.fxml", "Welcome!", null);
+            }
+        });
+////but de a merge inapoi la pagina cu aplicatii.
+
+        button_back.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+//                search_user();
+//                UpdateTable();
+                Utils.changeScene(event, "Dashboard.fxml", "Welcome!", null);
+            }
+
+        });
+
     }
+
+
+
+
+
 }
